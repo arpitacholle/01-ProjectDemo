dc73313c102b71afdafd1fc034fbdd0ea0a50cbe
arpitacholle <arpitacholle@gmail.com>
2023-07-07 19:26:34 +0530

    Added the A.java Class

---
+++ A.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class A {
+	
+	public void m1() {
+		System.out.println("Inisde the Class A method m1");
+	}
+	
+	public static void m2() {
+		System.out.println("Inisde the Class A static method m2");
+	}
+}
